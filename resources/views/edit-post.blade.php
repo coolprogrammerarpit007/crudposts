13146e42e0a7f859b444b4562e07d7584dc58213
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-22 19:15:22 +0530

    created the form to edit data

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edit Blog Post</title>
+
+    {{-- Google Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playwrite+AR:wght@100..400&display=swap" rel="stylesheet">
+
+    {{-- External Styling Link --}}
+    <link rel="stylesheet" href="{{ url('css/blog-edit.css') }}">
 </head>
+
 <body>
-    <h1>Blog Post Edit</h1>
+    <div class="container">
+        <form action="{{ url('/') }}/register" class="form-container" method="POST">
+            @csrf
+            <h2 class="register-title">Edit your form here...</h2>
+            <div class="title-container input-container">
+                <label for="title">Edit your title:</label>
+                <input type="text" name="title" id="text" placeholder="Edit your title here...">
+            </div>
+            <div class="post-body input-container">
+                <label for="body">Edit your post message here</label>
+                <textarea name="body" id="body" cols="30" rows="10" placeholder="Edit your message here..."></textarea>
+            </div>
+            <div class="input-btns">
+                <input type="submit" value="Submit" id="submit">
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
